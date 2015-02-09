41cc066037dee28f7ab816fe1366ef401c824d25
Brandon Peters <brandon@suspend.io>
2015-02-09 14:24:06 +0000

    Killed roomqueue and roomroster models. Updated user model

--- lstn/models/__init__.py
+++ lstn/models/__init__.py
@@ -1,5 +1,6 @@
 import datetime
 import simplejson as json
+import rdio
 
 from lstn import db
 
@@ -40,14 +41,22 @@ class User(db.Model, ModelMixin, UserMixin):
   id = db.Column(db.BigInteger, primary_key=True)
   name = db.Column(db.String(255))
   external_id = db.Column(db.String(255))
+  profile = db.Column(db.String(255))
   oauth_token=db.Column(db.String(255))
   oauth_token_secret=db.Column(db.String(255))
+  queue = db.Column(db.String(255))
   picture = db.Column(db.String(32))
   points = db.Column(db.BigInteger)
   settings = db.Column(db.Text)
   created_at = db.Column(db.DateTime, nullable=False, default=db.func.now())
 
   owned = db.relationship('Room', backref='owner')
+
+  def is_anonymous(self):
+    return False
+
+  def to_json(self, for_public=False):
+    return json.dumps(self.to_array(for_public=for_public), cls=ModelEncoder)
 
   def to_array(self, for_public=False):
     data = super(User, self).to_array()
@@ -58,6 +67,67 @@ class User(db.Model, ModelMixin, UserMixin):
       data.pop('settings', None)
 
     return data
+
+  def get_queue_id(self):
+    if self.queue:
+      return self.queue
+
+    data = {
+      'method': 'getUserPlaylists',
+      'user': self.external_id,
+    };
+
+    rdio_manager = rdio.Api(current_app.config['RDIO_CONSUMER_KEY'],
+      current_app.config['RDIO_CONSUMER_SECRET'],
+      self.oauth_token,
+      self.oauth_token_secret)
+
+    results = rdio_manager.call_api_authenticated(data);
+    if results:
+      for playlist in results:
+        if playlist['name'] == u'Lstn to Rdio':
+          self.queue = playlist['key']
+          break
+
+    if not self.queue:
+      return None
+
+    db.session.add(self)
+    db.session.commit()
+
+    return self.queue
+
+  def get_queue(self):
+    rdio_manager = rdio.Api(current_app.config['RDIO_CONSUMER_KEY'],
+      current_app.config['RDIO_CONSUMER_SECRET'],
+      self.oauth_token,
+      self.oauth_token_secret)
+
+    queue_id = self.get_queue_id()
+    if not queue_id:
+      return []
+
+    current_app.logger.debug('queue id %s' % queue_id)
+
+    try:
+      playlists = rdio_manager.get([queue_id], ['tracks'])
+    except Exception as e:
+      current_app.logger.debug(e)
+      return []
+
+    if not playlists:
+      return []
+
+    playlist = playlists[0]
+
+    queue = []
+    for track in playlist.tracks:
+      data = track._data
+      data['in_queue'] = 1
+
+      queue.append(data)
+
+    return queue
 
   @staticmethod
   def get_users(include_self=True):
@@ -78,50 +148,3 @@ class Room(db.Model, ModelMixin):
   owner_id = db.Column(db.BigInteger, db.ForeignKey('user.id'))
   settings = db.Column(db.Text)
   created_at = db.Column(db.DateTime, nullable=False, default=db.func.now())
-
-  def get_queue(self):
-    room_queue = RoomQueue.query.\
-      filter(RoomQueue.room_id == self.id).\
-      order_by(RoomQueue.created_at.asc()).\
-      all()
-
-    tracks = []
-    for entry in room_queue:
-      data = json.loads(entry.track)
-      data['id'] = entry.id
-      tracks.append(data)
-
-    return tracks
-
-  def add_track(self, song_id, track):
-    room_queue = RoomQueue(
-      room_id=self.id,
-      song_id=song_id,
-      user_id=current_user.id,
-      track=json.dumps(track)
-    )
-
-    db.session.add(room_queue)
-    db.session.commit()
-
-class RoomQueue(db.Model, ModelMixin):
-  __tablename__ = 'roomQueue'
-  id = db.Column(db.BigInteger, primary_key=True)
-  room_id = db.Column(db.BigInteger, db.ForeignKey('room.id'))
-  song_id = db.Column(db.String(255))
-  user_id = db.Column(db.BigInteger, db.ForeignKey('user.id'))
-  track = db.Column(db.Text)
-  created_at = db.Column(db.DateTime, nullable=False, default=db.func.now())
-
-  room = db.relationship(Room, backref='queue')
-
-class RoomRoster(db.Model, ModelMixin):
-  __tablename__ = 'roomRoster'
-
-  id = db.Column(db.BigInteger, primary_key=True)
-  room_id = db.Column(db.BigInteger, db.ForeignKey('room.id'))
-  user_id = db.Column(db.BigInteger, db.ForeignKey('user.id'))
-  created_at = db.Column(db.DateTime, nullable=False, default=db.func.now())
-
-  room = db.relationship(Room, backref='roster')
-  user = db.relationship(User, backref='memberships')
