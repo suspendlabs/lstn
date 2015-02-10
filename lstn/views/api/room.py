44286372115bf5d9b5ba1c1c37cc96da962dce6c
Brandon Peters <brandon@suspend.io>
2015-02-10 08:46:10 +0000

    Added redis to query recent rooms

--- lstn/views/api/room.py
+++ lstn/views/api/room.py
@@ -1,6 +1,7 @@
 import string
 import simplejson as json
 import rdio
+import time
 
 from flask import Flask, request, redirect, url_for, \
   render_template, Blueprint, current_app, jsonify
@@ -8,7 +9,7 @@ from flask import Flask, request, redirect, url_for, \
 from flask.ext.login import login_required, current_user
 from slugify import slugify
 
-from lstn import db
+from lstn import db, r
 from lstn.models import Room
 from lstn.exceptions import APIException
 
@@ -20,15 +21,29 @@ def room_action():
   if request.method == 'POST':
     return room_post_action()
 
-  rooms = []
+  # Get the rooms owned by the current user
+  rooms = {}
   for room in current_user.owned:
     data = room.to_array()
     data['owner'] = room.owner.to_array(for_public=True)
-    rooms.append(data)
+    rooms[data['id']] = data;
+
+  # Fetch recent rooms from Redis
+  userKey = 'user_%s' % current_user.id
+  start_time = int(time.time()) - (60 * 60 * 24 * 7);
+  recent_room_ids = r.zrevrangebyscore(userKey, '+inf', start_time)
+  recent_room_ids = [room_id for room_id in recent_room_ids if room_id not in rooms]
+
+  if recent_room_ids:
+    recent_rooms = Room.query.filter(Room.id.in_(recent_room_ids)).all()
+    for room in recent_rooms:
+      data = room.to_array()
+      data['owner'] = room.owner.to_array(for_public=True)
+      rooms[data['id']] = data
 
   response = {
     'success': 1,
-    'rooms': rooms,
+    'rooms': rooms.values()
   }
 
   return jsonify(response)
