355d698db6f35e5d088307bb017bb4def4b957ad
Brandon Peters <brandon@formstack.com>
2015-02-20 00:48:52 -0500

    Fix bad reference for upvote. Fix score issue when downvoting

--- lstn/static/js/controllers.js
+++ lstn/static/js/controllers.js
@@ -312,7 +312,7 @@ angular.module('lstn.controllers', [])
           $scope.playing.downvotes = {};
         }
 
-        $scope.playing.downvotes[upvote.user] = true;
+        $scope.playing.downvotes[downvote.user] = true;
       }
 
       if (!$scope.isCurrentController) {
@@ -332,7 +332,7 @@ angular.module('lstn.controllers', [])
         console.log('CurrentUser.get', response);
       });
 
-      if (score <= -2) {
+      if (downvote.votes <= -2) {
         $scope.skipSong();
       }
     });
