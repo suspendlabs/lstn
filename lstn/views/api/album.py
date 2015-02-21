a5387f2f330f1c9860f9d92532b44b0fdd43ab36
Brandon Peters <brandon.peters@gmail.com>
2015-02-21 13:59:04 -0500

    Added api endpoints for album and artist

---
+++ lstn/views/api/album.py
@@ -0,0 +1,36 @@
+import simplejson as json
+import rdio
+
+from flask import Flask, request, redirect, url_for, \
+  render_template, Blueprint, current_app, jsonify
+
+from flask.ext.login import login_required, current_user
+
+from lstn import db
+
+album = Blueprint('album', __name__, url_prefix='/api/album')
+
+@album.route('/<album_id>/tracks', methods=['GET'])
+@login_required
+def get_tracks(album_id):
+  rdio_manager = rdio.Api(current_app.config['RDIO_CONSUMER_KEY'],
+    current_app.config['RDIO_CONSUMER_SECRET'],
+    current_user.oauth_token,
+    current_user.oauth_token_secret)
+
+  tracks = []
+
+  albums = rdio_manager.get([album_id], ['tracks'])
+  if len(albums) > 0:
+    if len(albums[0].track_keys) > 0:
+      tracks = rdio_manager.get(albums[0].track_keys)
+
+      if len(tracks) > 0:
+        tracks = [track._data for track in tracks]
+
+  response = {
+    'success': 1,
+    'tracks': tracks,
+  }
+
+  return jsonify(response)
---
+++ lstn/views/api/artist.py
@@ -0,0 +1,30 @@
+import simplejson as json
+import rdio
+
+from flask import Flask, request, redirect, url_for, \
+  render_template, Blueprint, current_app, jsonify
+
+from flask.ext.login import login_required, current_user
+
+from lstn import db
+
+artist = Blueprint('artist', __name__, url_prefix='/api/artist')
+
+@artist.route('/<artist_id>/albums', methods=['GET'])
+@login_required
+def get_albums(artist_id):
+  rdio_manager = rdio.Api(current_app.config['RDIO_CONSUMER_KEY'],
+    current_app.config['RDIO_CONSUMER_SECRET'],
+    current_user.oauth_token,
+    current_user.oauth_token_secret)
+
+  albums = rdio_manager.get_albums_for_artist(artist_id)
+  if len(albums) > 0:
+      albums = [album._data for album in albums]
+
+  response = {
+    'success': 1,
+    'albums': albums,
+  }
+
+  return jsonify(response)
